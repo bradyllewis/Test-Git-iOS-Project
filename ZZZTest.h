1d312ee360329dd8ea795a5da17a9b8e4938319e
Brady Lewis <bradyllewis@Bradys-Mac-mini.local>
2014-04-30 12:41:40 -0400

    added a test class

---
+++ ZZZTest.h
@@ -0,0 +1,13 @@
+//
+//  ZZZTest.h
+//  Git Test
+//
+//  Created by Brady Lewis on 4/30/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZZTest : NSObject
+
+@end
---
+++ ZZZTest.m
@@ -0,0 +1,13 @@
+//
+//  ZZZTest.m
+//  Git Test
+//
+//  Created by Brady Lewis on 4/30/14.
+//
+//
+
+#import "ZZZTest.h"
+
+@implementation ZZZTest
+
+@end
